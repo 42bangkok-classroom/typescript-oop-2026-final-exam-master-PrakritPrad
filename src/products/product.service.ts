5d6d578bb7b2cdc387b833be34f87da803d3d29f
Prakrit <poom66310@gmail.com>
2026-03-25 10:44:58 +0700

    2.2

--- src/products/product.service.ts
+++ src/products/product.service.ts
@@ -4,7 +4,7 @@ import * as fs from 'fs';
 @Injectable()
 export class ProductService {
   findAll() {
-    const data = fs.readFileSync('data/missions.json', 'utf8');
+    const data = fs.readFileSync('data/products.json', 'utf8');
     const missions = JSON.parse(data) as IProduct[];
     return {
       success: true,
