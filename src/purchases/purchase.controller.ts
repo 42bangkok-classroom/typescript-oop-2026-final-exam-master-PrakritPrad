f1c472c4f15c8d8048dca7172105069495e86596
Prakrit <poom66310@gmail.com>
2026-03-25 11:33:08 +0700

    3.2

--- src/purchases/purchase.controller.ts
+++ src/purchases/purchase.controller.ts
@@ -1,7 +1,7 @@
 import { Body, Controller, Get } from '@nestjs/common';
 import { PurchaseService } from './purchase.service';
 
-@Controller('Purchases')
+@Controller('purchases')
 export class PurchaseController {
   constructor(private readonly purchaseService: PurchaseService) {}
 
